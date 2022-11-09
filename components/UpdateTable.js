1b672be7c12b1187e8552cd6c687708b28c70fc4
Jean Juste <jrjuste@gmail.com>
2022-11-08 19:58:18 -0500

    adding html

--- components/UpdateTable.js
+++ components/UpdateTable.js
@@ -2,6 +2,27 @@ import styles from './UpdateTable.module.css'
 
 export default function UpdateTable() {
     return (
-        <p>Testing</p>
+
+        
+        
+        <table border="1px">
+            <thead>
+                <tr>
+                    <th>Data Collection</th>
+                </tr>
+                <tr>
+                    <th>Name</th>
+                    <th>Email</th>
+                    <th>Phone</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+            </tbody>
+        </table>
     )
 }

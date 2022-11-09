a1ab6d1984c87598997e2006cd1354edc3c8bbbd
Juste <jean.juste@its.ny.gov>
2022-11-09 10:22:04 -0500

    testing html

--- pages/table.js
+++ pages/table.js
@@ -2,9 +2,15 @@
 import UpdateTable from "@components/UpdateTable";
 
 export default function table () {
-    return <p>Testing Table page</p>
-
+    return `<p>Testing Table page</p>
     
+    <h1>Testing adding html</h1>
+    
+    `
+
+   
+
+
 }
 
 <UpdateTable />
